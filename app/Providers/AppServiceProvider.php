e29176e456e6ff27897fa7c589135dc275a7933e
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-02-05 08:45:52 +0700

    fix the bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Impl\TodolistServiceImpl;
+use App\Services\TodolistService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TodolistService::class, TodolistServiceImpl::class);
     }
 
     /**
--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -9,39 +9,32 @@ class TodolistServiceImpl implements TodoListService
 {
     public function saveTodo(string $id, string $todo): void
     {
-        // save data in sesssion (there is a !session if not execution this )
-        if (!Session::exists("todoList")) {
+        if (!Session::exists("todolist")) {
             Session::put("todolist", []);
         }
 
-        // insert session data in array
         Session::push("todolist", [
             "id" => $id,
             "todo" => $todo
         ]);
     }
 
-    // take array data in session
     public function getTodolist(): array
     {
-        // get data in todo list , if not take array 0 value
         return Session::get("todolist", []);
     }
 
-    // remove data
     public function removeTodo(string $todoId)
     {
         $todolist = Session::get("todolist");
 
         foreach ($todolist as $index => $value) {
-            // if value from id same DELETE
-            if ($value['id'] == $todolist) {
+            if ($value['id'] == $todoId) {
                 unset($todolist[$index]);
                 break;
             }
         }
 
-        // save session if deleted done
         Session::put("todolist", $todolist);
     }
 }
